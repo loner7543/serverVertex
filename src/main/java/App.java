9e818e410a579479841a4f2f42be4a633298b935
loner7543 <Sachok1394@mail.ru>
2017-05-26 20:18:00 +0400

    add map

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,99 +2,100 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.impl.ConcurrentHashSet;
-import io.vertx.core.impl.launcher.commands.ExecUtils;
 
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Set;
+import java.util.TreeMap;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    static private Object key=new Object();
-    static private Set<ServerWebSocket> clients = new ConcurrentHashSet<>();
-    static private TreeMap<Long,String> messages  = new TreeMap<>((o1,o2)->{
+    private static final int MSG_BLOCK_SIZE=50;
+    private static final String  WS_URL_TAIL = "/ws";//http
+    private static final String ADD_MES_TAIL = "/api/msg";
+    private static Object key=new Object();
+    private static Set<ServerWebSocket> allUsrs = new ConcurrentHashSet<>();
+     private static TreeMap<Long,String> allMessages = new TreeMap<>((o1, o2)->{
         return Long.compare(o2,o1);
     });
-    private static final int MSG_BLOCK_SIZE=50;
-    public static void main( String[] args )
-    {
-       final Vertx vertx = Vertx.vertx();
-       final HttpServer server = vertx.createHttpServer();
 
 
-       server.requestHandler(request -> {
-           String path = request.path();
-           switch (path){
-               case "/ws":
-                   ServerWebSocket webSocket = request.upgrade();
-                   clients.add(webSocket);
-                   webSocket.textMessageHandler(msg->{
-                       putMessage(msg);
-                       clients.forEach(client->{
-                           client.writeTextMessage(msg);
-                       });
-                       System.out.println("[NEW MESSAGE]:"+msg);
-                   });
-                   webSocket.endHandler(aVoid->{
-                       clients.remove(webSocket);
-                   });
-                   webSocket.closeHandler(aVoid -> {
-                       clients.remove(webSocket);
-                   });
-                   break;
+    public static void main( String[] args ) {
+        Vertx core = Vertx.vertx();
+        HttpServer httpServer = core.createHttpServer();
+        System.out.println("Server created");
+
+
+        httpServer.requestHandler(request -> {//обр зап
+            String tail = request.path();//
+            switch (tail) {
+                case WS_URL_TAIL:
+                    ServerWebSocket socket = request.upgrade();
+                    allUsrs.add(socket);// извери - это сокет
+                    socket.textMessageHandler(msg -> {
+                        putMessage(msg);
+                        allUsrs.forEach(client -> {
+                            client.writeTextMessage(msg);
+                        });
+                    });
+                    socket.endHandler(aVoid -> {
+                        allUsrs.remove(socket);
+                    });
+                    socket.closeHandler(aVoid -> {
+                        allUsrs.remove(socket);
+                    });
+                    break;
                /*-----------------------------------------------------*/
-               case "/api/msg":
-                   String responseString=null;
-                   try {
-                       String svalue = request.getParam("from");
-                       Long value = Long.parseLong(svalue);
-                       responseString = getMessages(value);
-                   }catch(Exception e){
-                       responseString="[]";
-                   }
-                   request.response()
-                           .putHeader("Access-Control-Allow-Origin","*")
-                           .end(responseString);
-                   break;
-               default:
-                   request.response().setStatusCode(404).end();
-                   break;
+                case ADD_MES_TAIL:
+                    String resp = null;
+                    try {
+                        String messageBody = request.getParam("from");
+                        Long value = Long.parseLong(messageBody);
+                        resp = getMessages(value);
+                    } catch (Exception e) {
+                        resp = "[]";
+                    }
+                    request.response()
+                            .putHeader("Access-Control-Allow-Origin", "*")
+                            .end(resp);
+                    break;
+                default:
+                    request.response().setStatusCode(404).end();
+                    break;
 
-           }
+            }
 
-       }).listen(8080);
+        }).listen(8080);
+        System.out.println("Starting");
+
+
+    }
+
+    public static String getMessages(long end){
+        synchronized (key){// зверей мб много
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("[");
+            int count=0;
+            Set<Long> keys = allMessages.subMap(end,false,0L,false).keySet();
+
+            for(Long key: keys){
+                String msg = allMessages.get(key);
+                stringBuilder.append(msg);
+                count++;
+                if(count==MSG_BLOCK_SIZE || count == keys.size()){
+                    break;
+                }
+                stringBuilder.append(",");
+            }
+            stringBuilder.append("]");
+            return stringBuilder.toString();
+        }
 
 
     }
     public static void putMessage(String message){
         synchronized(key){
-           messages.put(System.currentTimeMillis(),message);
+            allMessages.put(System.currentTimeMillis(),message);
         }
     }
 
-    public static String getMessages(long end){
-        synchronized (key){
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            int count=0;
-            Set<Long> keys = messages.subMap(end,false,0L,false).keySet();
-
-            for(Long key: keys){
-                String msg = messages.get(key);
-                sb.append(msg);
-                count++;
-                if(count==MSG_BLOCK_SIZE || count == keys.size()){
-                    break;
-                }
-                sb.append(",");
-            }
-            sb.append("]");
-            return sb.toString();
-        }
-
-
-    }
 }
